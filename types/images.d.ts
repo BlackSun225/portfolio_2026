0b3309eedf3a8aee8ba7fc9f378f73c86c2881a7
BlackSun225 <yohananchris@outlook.com>
2026-04-11 20:41:14 +0000

    i updated image type to use StaticImageData instead of string

--- types/images.d.ts
+++ types/images.d.ts
@@ -1,30 +1,32 @@
+import { StaticImageData } from "next/image"
+
 // types/images.d.ts
 declare module '*.svg' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
 
 declare module '*.png' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
 
 declare module '*.jpg' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
 
 declare module '*.jpeg' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
 
 declare module '*.gif' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
 
 declare module '*.webp' {
-  const content: string
+  const content: StaticImageData
   export default content
 }
