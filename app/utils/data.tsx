4b288f132318bbdaa4da61a459cdad3ad7ce02a6
BlackSun225 <yohananchris@outlook.com>
2026-04-12 18:22:26 +0000

    UX improvement in navbar

--- app/utils/data.tsx
+++ app/utils/data.tsx
@@ -13,7 +13,7 @@ export const dictionary = {
             en: "SERVICES"
         },
         pricing: {
-            fr: "TARIF",
+            fr: "BUDGET",
             en: "PRICING"
         }
     },
