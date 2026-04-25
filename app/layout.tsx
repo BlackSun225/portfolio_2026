ec940ffd375c5d63fa1c534d5ed6740ffe9c5fa1
BlackSun225 <yohananchris@outlook.com>
2026-04-25 20:33:30 +0000

    i added clarity script to track my website usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import NavContextComponent from "./components/navContext";
+import Script from "next/script";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -72,6 +73,16 @@ export default function RootLayout({
           {children}
           <Footer />
         </NavContextComponent>
+        {/* Microsoft Clarity Script */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "whdvdru650");
+          `}
+        </Script>
       </body>
     </html>
   );
