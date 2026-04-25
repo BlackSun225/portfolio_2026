4ccddf01730dedd5a6a1a84799daa7b56b976f72
BlackSun225 <yohananchris@outlook.com>
2026-04-25 16:48:59 +0000

    i added more path to sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,25 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://maisondoudjo.com'
   return [
     {
-      url: 'https://maisondoudjo.com',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/work`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     // Add your project URLs here
   ]
