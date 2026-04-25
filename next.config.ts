3d0a5dbd445d1ca92729a5d0d73c6dd19d1dabcc
BlackSun225 <yohananchris@outlook.com>
2026-04-25 17:24:08 +0000

    i added a .env variable for image server host

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,40 @@
 import type { NextConfig } from "next";
-const IMAGE_SERVER_URL = process.env.IMAGE_SERVER_URL || "localhost:2700/"
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+const IMAGE_SERVER_HOST = process.env.IMAGE_SERVER_HOST || "localhost"
+
+const localRemotePatterns: RemotePattern[] = [
+  {
+    protocol: 'http',
+    hostname: IMAGE_SERVER_HOST,
+    port: '3001',
+    pathname: '/images/**',
+  },
+  {
+    protocol: 'http',
+    hostname: IMAGE_SERVER_HOST,
+    port: '3000',
+    pathname: '/images/**',
+  },
+  {
+    protocol: 'http',
+    hostname: IMAGE_SERVER_HOST,
+    port: '2700',
+    pathname: '/images/**',
+  }
+]
+
+const productionRemotePatterns: RemotePattern[] = [
+  {
+    protocol: 'https',
+    hostname: IMAGE_SERVER_HOST,
+    pathname: '/images/**',
+  }
+];
 
 const nextConfig: NextConfig = {
   images: {
     dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: IMAGE_SERVER_URL,
-        // port: '3001',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '2700',
-        pathname: '/images/**',
-      }
-    ],
+    remotePatterns: process.env.NODE_ENV === "development" ? localRemotePatterns : productionRemotePatterns,
     // Configure device sizes for responsive images
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
