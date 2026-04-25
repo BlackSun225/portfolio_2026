99672b6d01731f9be8030094cc0a786de8d6ded2
BlackSun225 <yohananchris@outlook.com>
2026-04-25 16:50:28 +0000

    i added more path to robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/work', '/services'],
       disallow: '/portfolio/', // Block folders you don't want indexed
     },
     sitemap: 'https://maisondoudjo.com/sitemap.xml',
