bdf07d695ddbe2c656272b6bd5ef3e09ac70809b
BlackSun225 <yohananchris@outlook.com>
2026-05-25 18:32:04 +0000

    i added new page url to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -21,6 +21,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/pricing`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/mail`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
     // Add your project URLs here
   ]
 }
